e9e91db95a6f057229ed3a0a413e41cf1ddf910f
sofien benrhouma <sofien.benrhouma@ausy.com>
2023-05-25 02:28:29 +0100

    finishing

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+
+    #[Route(path: '/', name: 'app_index')]
+    public function index(): Response
+    {
+        $user = $this->getUser();
+
+        return $this->render('app/index.html.twig', ['user' => $user]);
+    }
+
+
+
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+         if ($this->getUser()) {
+             return $this->redirectToRoute('app_index');
+         }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
